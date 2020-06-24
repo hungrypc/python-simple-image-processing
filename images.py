ad984a9a7761ad16004d6364d58f04f84c13a231
hpc <philipchanhk626@gmail.com>
2020-06-24 09:15:35 -0400

    show(), rotate(), resize()

--- images.py
+++ images.py
@@ -12,4 +12,13 @@ img = Image.open('./pokedex/pikachu.jpg')
 # filtered_img.save("sharpen.png", 'png')
 
 filtered_img = img.convert('L')   # converts format to greyscale
-filtered_img.save("grey.png", 'png')
+# filtered_img.save("grey.png", 'png')
+
+# filtered_img.show()   # opens img
+
+# crooked = filtered_img.rotate(90)
+# crooked.save('crooked_grey.png', 'png')
+
+resized_img = filtered_img.resize((300, 300))
+resized_img.save('resized_grey.png', 'png')
+
